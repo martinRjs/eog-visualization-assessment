5e717a543dbc97314e9a48d3660e93ea8104c168
Martin Rojas <martin.rojas.gz@gmail.com>
2019-09-17 15:25:06 -0500

    remove debugger, add missing semicolons

--- src/store/localStorage.js
+++ src/store/localStorage.js
@@ -1,11 +1,10 @@
 export const saveState = (state) => {
-  debugger;
   let serializedData = JSON.stringify(state);
-  localStorage.setItem('state', serializedData)
+  localStorage.setItem('state', serializedData);
 }
 
 export const loadState = () => {
-  const serializedData = localStorage.getItem('state')
+  const serializedData = localStorage.getItem('state');
   if (serializedData === null) {
     return undefined;
   }
